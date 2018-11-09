fbafe203ec74ef618e28cb691e691e6beba3af53
Tianna Wen <tianna.wen@lic.co.nz>
2018-11-09 16:02:47 +1300

    Create Animal route

--- src/routes/animal.ts
+++ src/routes/animal.ts
@@ -1,18 +1,32 @@
 import * as express from "express";
 import { MakeRepresentation } from "@licnz/rest";
 
-import * as data from "../farm.json";
+import * as data from "../animal.json";
+import { Uri } from "@licnz/uri";
 
 const maker = new MakeRepresentation("http://localhost:3000");
 
 export const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.status(200).end();
+  res.send(
+    maker.makeFeedRepresentation(
+      data.animals,
+      "name",
+      [{ rel: "self", href: "/animals" }],
+      new Uri("/animals/:id", "animal")
+    )
+  );
 });
 
 router.get("/:id", (req, res) => {
-  res.send("animal").end();
+  const id = Number(req.params.id);
+  const animal = data.animals.filter(animal => animal.id === id)[0];
+  res.send(
+    maker.makeLinkedRepresentation(animal, [
+      { rel: "self", href: `/animals/${id}` },
+      { rel: "up", href: "/animals" },
+      { rel: "farm", href: "/farms" }
+    ])
+  );
 });
-
-router.get("/");
